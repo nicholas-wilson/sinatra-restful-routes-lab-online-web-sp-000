5a29a060fc61e3f1805fadc718255cd71382ac9c
Nicholas Wilson <nicholas.wilson@nevada.unr.edu>
2020-02-10 05:26:57 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,40 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
 
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    recipe = Recipe.create(params)
+    redirect "/recipes/#{recipe.id}"
+  end
+
+  get '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  delete '/recipes/:id' do
+    recipe = Recipe.find(params[:id])
+    recipe.destroy
+    redirect '/recipes'
+  end
+
+  patch '/recipes/:id' do
+    @recipe = Recipe.find(params[:id])
+    new_info = {name: params[:name], cook_time: params[:cook_time], ingredients: params[:ingredients]}
+    @recipe.update(new_info)
+    redirect "/recipes/#{@recipe.id}"
+  end
 end
